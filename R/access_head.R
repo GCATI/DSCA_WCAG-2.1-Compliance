2d79b870e8f9e134424dacf5c987508937facf45
r-leyshon <richard.leyshon@ons.gov.uk>
2021-07-04 20:35:04 +0100

    feat: Stop access_head if rmd output is not html

--- R/access_head.R
+++ R/access_head.R
@@ -1,11 +1,13 @@
 #' access_head
 #'
-#' @param rmd_path Path to the Rmd that requires accessible header metadata.
+#' @param rmd_path Path to the Rmd that requires accessible header metadata. Tmd
+#' must be output type html.
+#' 
 #' @param replace Defaults to FALSE. Should the YAML be replaced or
 #' supplemented.
 #' 
 #' @return Adjust the Rmd YAML provided to `rmd_path`, improving its
-#' accessibility for screen readers.
+#' accessibility for screen readers. Only works with html output.
 #' 
 #' @importFrom htmltools tags withTags
 
@@ -49,15 +51,21 @@ access_head <- function(rmd_path = NULL, replace = FALSE){
   # Will need to identify YAML elements present and convert to html flexibly
   # remove YAML bounds "---"
   head <- setdiff(yaml_head, "---")
+  # check for html output type. Stop if not.
+  html_loc <- grepl(pattern = "^output: html_document$", head)
+
+  if(any(html_loc)){
+    # html found. subset out the html tag.
+    head <- head[!html_loc]
+  } else{
+    stop("access_head() only works with html output.")
+  }
   # Clean out the escape chars
   # remove all besides the alphabets, numbers, : and /
   # finding a simple implementation to remove single escapes is elusive
   head <- gsub("[^A-Za-z0-9:/]", "", head)
-  
-  # create a list of head metadata
-  hd_meta <- 
-  
-  
+  # split the metadata on column and assign as named vector
+  meta_list <- str_split(head, pattern = ":")
   # find title
   hd_index <- grep("title:", head)
   
